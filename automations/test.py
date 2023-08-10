5d68e3e44ee2c39f826e125fc1a24bb3dba69013
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-08-10 08:20:36 +0530

    fixed exposed password, hehe😄

---
+++ automations/test.py
@@ -0,0 +1,28 @@
+from common import user
+from common.session import Session
+from helpers.dev_tools import *
+
+from private import simcobot
+
+import time
+
+bot = user.User(simcobot.username, simcobot.password)
+# current_session = Session(bot)
+
+# print(bot, current_session)
+
+# print("authenticating")
+# current_session.generate_cookies()
+# r = current_session.authenticate()
+# print(r, r.ok, r.json())
+# print("done")
+
+start_dev_mode()
+bot.generate_cookies()
+# print(1)
+# time.sleep(5)
+print(bot.authenticate().status_code)
+# print(2)
+# time.sleep(5)
+# bot.get_user_id_and_companies()
+# print(3)
